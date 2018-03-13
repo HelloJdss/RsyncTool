4563c7ede3589c2fd7d4582034d65c5cef63d32e
U201414571-Qin <382825415@qq.com>
2018-03-13 21:45:46 +0800

    log 0.1

---
+++ code/common/LogModule/LogHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xiao on 18-3-13.
+//
+
+#include "LogHelper.h"
---
+++ code/common/LogModule/LogHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by xiao on 18-3-13.
+//
+
+#ifndef RSYNCTOOL_LOGHELPER_H
+#define RSYNCTOOL_LOGHELPER_H
+
+
+class LogHelper {
+
+};
+
+
+#endif //RSYNCTOOL_LOGHELPER_H
